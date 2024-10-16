c4e59ba5586d01b8cc86ef0aa2c9a0a44f34a480
SahadeRenzo <sahaderenzo02@gmail.com>
2024-10-16 11:08:03 -0300

    Finalizando sexto video

--- main.js
+++ main.js
@@ -2,3 +2,54 @@ import { renderCategories } from "./src/services/categories";
 import './style.css';
 
 renderCategories();
+
+/*PRODUCTOS*/
+const buttonAdd = document.getElementById("buttonAddElement");
+buttonAdd.addEventListener('click', () => {
+    openModel();
+});
+
+
+/*POPUP*/
+const buttonCancel = document.getElementById("cancelButton");
+buttonCancel.addEventListener('click', () => {
+    handleButtonCancel();
+});
+
+const handleButtonCancel = () => {
+    closeModel();
+}
+
+//Funciones para abrir y cerrar model
+const openModel = () => {
+    const model = document.getElementById("modalPopUP");
+    model.style.display = "flex";
+}
+
+const closeModel = () => {
+    const model = document.getElementById("modalPopUP");
+    model.style.display = "none";
+}
+
+/*Guardar o modificar elementos*/
+const acceptButton = document.getElementById("acceptButton");
+
+acceptButton.addEventListener('click', () => {
+    handleSaveOrModifyElements();
+});
+
+const handleSaveOrModifyElements = () => {
+    const nombre = document.getElementById("nombre").value,
+        imagen = document.getElementById("img").value,
+        precio = document.getElementById("precio").value,
+        categories = document.getElementById("categoria").value;
+
+    console.log({
+        nombre,
+        imagen,
+        precio,
+        categories
+    });
+
+    closeModel();
+};
